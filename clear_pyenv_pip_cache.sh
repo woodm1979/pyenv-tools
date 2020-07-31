0765f40e6c661b38bf8d89d01779e2a85fc599e3
woodnt <mwood@redzone.co>
2020-07-31 11:32:27 -0600

    added a pip cache clearing script

---
+++ clear_pyenv_pip_cache.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export PIP_REQUIRE_VIRTUALENV=false
+
+for VERSION in $(pyenv versions --bare | grep -v '/usr/local/Cellar' | sed 's/\*//' | tr -s ' ' | cut -d ' ' -f 2); do
+    echo $VERSION
+    echo "purging pip cache for $VERSION"
+    export PYENV_VERSION=$VERSION
+    python --version
+    pip --version 
+    pyenv which pip
+    pip cache purge
+done
